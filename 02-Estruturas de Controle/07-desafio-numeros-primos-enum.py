6dc0e670ece8854d795491707214c98e70e4711b
David Sotto Mayor <ddsmmf@gmail.com>
2022-10-13 14:55:41 -0400

    Arrumando scripts dos desafios

---
+++ 02-Estruturas de Controle/07-desafio-numeros-primos-enum.py
@@ -0,0 +1,8 @@
+# Entre com um número e verifique se ele é primo
+
+# Números Primos -> Chamamos de número primo um número natural que possui dois
+# divisores: 1 e ele mesmo.
+
+# Dica: use um range de (1, n), onde n é o número de entrada
+
+numero = 10
---
+++ 02-Estruturas de Controle/08-desafio-divisores-enum.py
@@ -0,0 +1,12 @@
+# Entre com um número e devolva uma lista com seus divisores
+
+# Divisor -> Um número é divisor do outro quando temos uma divisão
+# exata, ou seja, quando ela não deixa resto.
+
+# Dica: use um range de (1, n), onde p é o número de entrada
+
+# Bônus: Verifique se o número de entrada é primo
+
+
+numero = 20
+lista = []
